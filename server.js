95252d5437955c55752e0e372b71df303f266cec
Lauren Ferrett <lferrett@gmail.com>
2022-01-13 22:06:52 -0500

    Pushed to heroku

--- server.js
+++ server.js
@@ -1,21 +1,26 @@
+// Variables/required files
 const express = require('express');
 const fs = require('fs');
 const uuid = require('./helpers/uuid');
 const path = require('path');
-const PORT = process.env.PORT || 3001;
 const app = express();
+
+const PORT = process.env.PORT || 3001;
 
 let NotesDB = require('./db/db.json');
 
+// Routes - HTML
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
+// GET
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
 
 app.get('/api/notes', (req, res) => res.json(NotesDB));
 
+// POST
 app.post('/api/notes', (req, res) => {
     let idNumber = uuid();
     let newNote = req.body;
@@ -27,3 +32,6 @@ app.post('/api/notes', (req, res) => {
 app.listen(PORT, () => {
   console.log(`App listening on PORT: ${PORT}`);
 });
+
+// Starts the app
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));

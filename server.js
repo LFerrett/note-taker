3a8652ced5f5159c7f7fa050769505e5f85fee0b
Lauren Ferrett <lferrett@gmail.com>
2022-01-13 21:33:43 -0500

    Added helpers folder and uuid.js file.

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const fs = require('fs');
-
-const PORT = process.env.PORT || 3001;
 const db = require('./db/db.json');
-
+const path = require('path');
+const PORT = process.env.PORT || 3001;
 const app = express();
+
+app.use(express.urlencoded({extended: true}));
 
 app.listen(PORT, () =>
   console.log(`Express server listening on port ${PORT}!`)

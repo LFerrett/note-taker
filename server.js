b68809e59c7970723260fa137f3cdf3e4f9472d5
Lauren Ferrett <lferrett@gmail.com>
2022-01-13 21:50:30 -0500

    updated server with get and post

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
 const express = require('express');
 const fs = require('fs');
 const db = require('./db/db.json');
-// const uuid = require('./helpers/uuid');
+const uuid = require('./helpers/uuid');
 const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.listen(PORT, () =>
-  console.log(`Express server listening on port ${PORT}!`)
-);
+app.use(express.static("public"));
+
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
+
+app.get('/api/notes', (req, res) => res.json(NotesDB));
+
+app.post('/api/notes', (req, res) => {
+    let idNumber = uuid();
+    let newNote = req.body;
+    newNote.id = idNumber;
+    NotesDB.push(newNote);
+    res.json(NotesDB);
+});
 
 app.listen(PORT, () => {
   console.log(`App listening on PORT: ${PORT}`);

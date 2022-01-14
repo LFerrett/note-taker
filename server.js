35513d9aee09f6ecdcf8d2c82b222328dd46f5da
Lauren Ferrett <lferrett@gmail.com>
2022-01-13 21:39:01 -0500

    updated server with urlencoded support.

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const fs = require('fs');
 const db = require('./db/db.json');
+// const uuid = require('./helpers/uuid');
 const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
 app.listen(PORT, () =>
   console.log(`Express server listening on port ${PORT}!`)
 );
+
+app.listen(PORT, () => {
+  console.log(`App listening on PORT: ${PORT}`);
+});
